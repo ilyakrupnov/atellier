f3fd0166da111e06f06bdfca52dfea8ed5b88708
i.krupnov <i.krupnov@redmadrobot.com>
2017-11-15 16:12:13 +0300

    +PropTypes

--- src/PropertiesContainer.js
+++ src/PropertiesContainer.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import FieldType from './FieldType';
 
 class SimpleElement extends React.Component {
